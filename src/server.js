bc0277ef29477d36667feef0107c72e7f0c40c22
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-06 14:01:50 -0300

    create not found page with css, add main css and fixed route on server.js

--- src/server.js
+++ src/server.js
@@ -9,12 +9,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
 app.use((req, res) => {
-  res.send('Page not found');
+  res.render('not_found', {
+    title: '404! Page not found',
+  });
 });
 
 
